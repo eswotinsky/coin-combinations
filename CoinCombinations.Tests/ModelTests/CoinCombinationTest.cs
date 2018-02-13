27ffe6017161709c2e29a7aecd5854450b97d134
Eric Swotinsky and Faiza Husain <eric@>
2018-02-13 13:44:09 -0800

    Add logic to ModelsTest and they all failed successfully and then passed when code was added.

--- CoinCombinations.Tests/ModelTests/CoinCombinationTest.cs
+++ CoinCombinations.Tests/ModelTests/CoinCombinationTest.cs
@@ -21,10 +21,67 @@ namespace CoinCombinations.TestTools
     }
 
     [TestMethod]
-    public void ReturnCoinTypes_InputIsOnePenny_True()
+    public void ReturnCoinTypes_InputIsOneCent_True()
     {
       CoinCombination testAmount = new CoinCombination("1");
-      Assert.AreEqual("1 pennies", testAmount.ReturnCoinTypes());
+      Assert.AreEqual("0 quarters, 0 dimes, 0 nickels, 1 pennies", testAmount.ReturnCoinTypes());
     }
+
+    [TestMethod] //this will break soon. will need to update
+    public void ReturnCoinTypes_InputIsEightCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("8");
+      Assert.AreEqual("0 quarters, 0 dimes, 1 nickels, 3 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsFiveCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("5");
+      Assert.AreEqual("0 quarters, 0 dimes, 1 nickels, 0 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsTenCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("10");
+      Assert.AreEqual("0 quarters, 1 dimes, 0 nickels, 0 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsTwentyCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("20");
+      Assert.AreEqual("0 quarters, 2 dimes, 0 nickels, 0 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsTwentyThreeCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("23");
+      Assert.AreEqual("0 quarters, 2 dimes, 0 nickels, 3 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsTwentyFiveCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("25");
+      Assert.AreEqual("1 quarters, 0 dimes, 0 nickels, 0 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsFiftyCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("50");
+      Assert.AreEqual("2 quarters, 0 dimes, 0 nickels, 0 pennies", testAmount.ReturnCoinTypes());
+    }
+
+    [TestMethod]
+    public void ReturnCoinTypes_InputIsFiftyNineCents_True()
+    {
+      CoinCombination testAmount = new CoinCombination("59");
+      Assert.AreEqual("2 quarters, 0 dimes, 1 nickels, 4 pennies", testAmount.ReturnCoinTypes());
+    }
+
   }
 }
